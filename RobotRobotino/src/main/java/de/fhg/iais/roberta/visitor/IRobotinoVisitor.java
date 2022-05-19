6a74d5da94eaabc3d5d1f1cabd06f6cadab9fdc8
heini208 <mstemmeler@gmx.de>
2022-05-19 10:16:39 +0200

    first working code generation for robotino

--- RobotRobotino/src/main/java/de/fhg/iais/roberta/visitor/IRobotinoVisitor.java
+++ RobotRobotino/src/main/java/de/fhg/iais/roberta/visitor/IRobotinoVisitor.java
@@ -1,5 +1,7 @@
 package de.fhg.iais.roberta.visitor;
 
+import de.fhg.iais.roberta.syntax.action.OmnidriveAction;
+import de.fhg.iais.roberta.syntax.sensor.generic.TouchSensor;
 import de.fhg.iais.roberta.visitor.hardware.actor.IActors4AutonomousDriveRobots;
 import de.fhg.iais.roberta.visitor.hardware.sensor.ISensorVisitor;
 
@@ -7,5 +9,9 @@ import de.fhg.iais.roberta.visitor.hardware.sensor.ISensorVisitor;
  * Interface to be used with the visitor pattern to traverse an AST (and generate code, e.g.).
  */
 public interface IRobotinoVisitor<V> extends IActors4AutonomousDriveRobots<V>, ISensorVisitor<V> {
+
+    V visitTouchSensor(TouchSensor<V> touchSensor);
+
+    V visitOmnidriveAction(OmnidriveAction<V> omnidriveAction);
 
 }
