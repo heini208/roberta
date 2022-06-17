1420b4870d3c70c6366141cbe6c792a4ea3b4e53
heini208 <mstemmeler@gmx.de>
2022-06-17 16:40:19 +0200

    Added Validation and driveFor driveTo codegeneration

--- RobotRobotino/src/main/java/de/fhg/iais/roberta/visitor/IRobotinoVisitor.java
+++ RobotRobotino/src/main/java/de/fhg/iais/roberta/visitor/IRobotinoVisitor.java
@@ -3,6 +3,7 @@ package de.fhg.iais.roberta.visitor;
 import de.fhg.iais.roberta.syntax.action.OmnidriveAction;
 import de.fhg.iais.roberta.syntax.action.OmnidrivePositionAction;
 import de.fhg.iais.roberta.syntax.action.generic.PinWriteValueAction;
+import de.fhg.iais.roberta.syntax.action.motor.differential.MotorDriveStopAction;
 import de.fhg.iais.roberta.syntax.sensor.OdometryPosition;
 import de.fhg.iais.roberta.syntax.sensor.OdometryReset;
 import de.fhg.iais.roberta.syntax.sensor.generic.InfraredSensor;
@@ -14,11 +15,13 @@ import de.fhg.iais.roberta.visitor.hardware.sensor.ISensorVisitor;
 /**
  * Interface to be used with the visitor pattern to traverse an AST (and generate code, e.g.).
  */
-public interface IRobotinoVisitor<V> extends IActors4AutonomousDriveRobots<V>, ISensorVisitor<V> {
+public interface IRobotinoVisitor<V> extends ISensorVisitor<V> {
 
     V visitTouchSensor(TouchSensor<V> touchSensor);
 
     V visitOmnidriveAction(OmnidriveAction<V> omnidriveAction);
+
+    V visitMotorDriveStopAction(MotorDriveStopAction<Void> stopAction);
 
     V visitOmnidrivePositionAction(OmnidrivePositionAction<V> omnidrivePositionAction);
 
